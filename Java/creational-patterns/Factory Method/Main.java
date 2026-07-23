373df37389fc88d1588c716477025ca2f979e704
Luiz Paulo R. dos Santos <luizp36@yahoo.com>
2026-07-23 19:08:11 -0300

    Update Main.java

--- Java/creational-patterns/Factory Method/Main.java
+++ Java/creational-patterns/Factory Method/Main.java
@@ -1,9 +1,11 @@
 public class Main {
     public static void main(String[] args){
 
-        Carro carro = new ToyotaCorolla();
+        Carro carro1 = new ToyotaCorolla();
+        Carrp carro2 = new ToyotaHilux();
 
-        System.out.println(carro.descricao());
+        System.out.println(carro1.descricao());
+        System.out.println(carro2.descricao());
 
         System.out.println("OK!");
     }
