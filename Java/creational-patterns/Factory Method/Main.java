3ee6f479c68c1eb9ca25c7c9bce01e7a8107efa3
Luiz Paulo R. dos Santos <luizp36@yahoo.com>
2026-07-29 20:39:52 -0300

    Update Main.java

--- Java/creational-patterns/Factory Method/Main.java
+++ Java/creational-patterns/Factory Method/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args){
         
-        Carro carro1 = FactoryCarros.criarCarro(ModeloCarro.TOYOTA_COROLLA);
-        Carro carro2 = FactoryCarros.criarCarro(ModeloCarro.VW_GOLF);
-        Carro carro3 = FactoryCarros.criarCarro(ModeloCarro.FORD_MUSTANG);
-        Carro carro4 = FactoryCarros.criarCarro(ModeloCarro.FORD_RANGER);
-        Carro carro5 = FactoryCarros.criarCarro(ModeloCarro.VW_NIVUS);
-        Carro carro6 = FactoryCarros.criarCarro(ModeloCarro.TOYOTA_HILUX);
+        Carro carro1 = FactoryCarros.criarCarro(TOYOTA_COROLLA);
+        Carro carro2 = FactoryCarros.criarCarro(VW_GOLF);
+        Carro carro3 = FactoryCarros.criarCarro(FORD_MUSTANG);
+        Carro carro4 = FactoryCarros.criarCarro(FORD_RANGER);
+        Carro carro5 = FactoryCarros.criarCarro(VW_NIVUS);
+        Carro carro6 = FactoryCarros.criarCarro(TOYOTA_HILUX);
 
         System.out.println(carro1.descricao());
         System.out.println(carro2.descricao());
