5fd26e3877ec2b2de3066949ab3ae194ad07018c
Luiz Paulo R. dos Santos <luizp36@yahoo.com>
2026-08-03 20:25:32 -0300

    Update Carro.py

--- Python/Creational-Patterns/Factory-Method/Carro.py
+++ Python/Creational-Patterns/Factory-Method/Carro.py
@@ -6,25 +6,25 @@ class Carro(ABC):
     pass
   
 class ToyotaCorolla(Carro):
-  def descricao():
+  def descricao(self):
     print("Toyota Corolla - Sedan médio, famoso pela confiabilidade e conforto.")
 
 class ToyotaHilux(Carro):
-  def descricao():
+  def descricao(self):
     print("Toyota Hilux - Picape robusta, ideal para terrenos off-road.")
 
 class VolkswagenGolf(Carro):
-  def descricao():
+  def descricao(self):
     print("Volkswagen Golf - Hatchback esportivo com ótimo desempenho dinâmico.")
 
 class VolkswagenNivus(Carro):
-  def descricao():
+  def descricao(self):
     print("Volkswagen Nivus - SUV Cumpê urbano com design moderno e conectividade.")
 
 class FordMustang(Carro):
-  def descricao():
+  def descricao(self):
     print("Ford Mustang - Muscle car lendário com motor V8 potente.")
 
 class FordRanger(Carro):
-  def descricao():
+  def descricao(self):
     print("Ford Ranger - Picape média com alta capacidade de carga e tecnologia.")
