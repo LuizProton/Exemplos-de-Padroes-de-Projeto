5c7e0fcb3e460fee23eafe0ba3d9aa794db3ac5c
Luiz Paulo R. dos Santos <luizp36@yahoo.com>
2026-08-03 20:02:59 -0300

    Create Main.py

---
+++ Python/Creational-Patterns/Factory-Method/Main.py
@@ -0,0 +1,15 @@
+from Carro import *
+
+carro1 = ToyotaCorolla()
+carro2 = ToyotaHilux()
+carro3 = VolkswagenGolf()
+carro4 = VolkswagenNivus()
+carro5 = FordMustang()
+carro6 = FordRanger()
+
+cara1.descricao()
+cara2.descricao()
+cara3.descricao()
+cara4.descricao()
+cara5.descricao()
+cara6.descricao()
